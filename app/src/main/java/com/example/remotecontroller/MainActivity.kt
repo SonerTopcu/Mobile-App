0ad8d4ce17190ea831ee2122d6b73fb9503ac653
Soni <sonertopcu23@gmail.com>
2022-07-14 17:39:19 +0300

    Update

--- app/src/main/java/com/example/remotecontroller/MainActivity.kt
+++ app/src/main/java/com/example/remotecontroller/MainActivity.kt
@@ -1,76 +1,41 @@
 package com.example.remotecontroller
 
-
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
-
-
+import androidx.appcompat.app.AppCompatActivity
+import com.example.remotecontroller.manager.MQTTmanager
+import com.example.remotecontroller.protocols.UIUpdaterInterface
 import org.eclipse.paho.android.service.MqttAndroidClient
-import org.eclipse.paho.client.mqttv3.*
-import org.eclipse.paho.client.mqttv3.internal.wire.MqttConnect
 
-class MainActivity: AppCompatActivity(){
+
+class MainActivity : AppCompatActivity(), UIUpdaterInterface {
+
+    var mqtTmanager:MQTTmanager? = null
+    var brokerInf = findViewById<EditText>(R.id.brokerField)
+
+    override fun resetUIWithConnection(status: Boolean) {
+
+
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         lateinit var mqttClient: MqttAndroidClient
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_login)
+        setContentView(R.layout.activity_main)
 
-        val admin = "siyah"
-        val adminPwd = "siyaharge1"
-        var username = findViewById<EditText>(R.id.text_username)
-        var password = findViewById<EditText>(R.id.text_password)
-        var login = findViewById<Button>(R.id.btn_login)
-        var sample = findViewById<EditText>(R.id.text_sample)
-
-        login.setOnClickListener {
-            sample.setText("Poki")
-
-                val serverURI = "tcp://broker.hivemq.com:1883"
-            mqttClient = MqttAndroidClient(applicationContext, serverURI, "kotlin_client")
-                mqttClient.setCallback(object : MqttCallback {
-                    override fun messageArrived(topic: String?, message: MqttMessage?) {
-                        Log.d(MainLogin.TAG, "Receive message: ${message.toString()} from topic: $topic")
-                    }
-
-                    override fun connectionLost(cause: Throwable?) {
-                        Log.d(MainLogin.TAG, "Connection lost ${cause.toString()}")
-                    }
-
-                    override fun deliveryComplete(token: IMqttDeliveryToken?) {
-
-                    }
-                })
-                val options = MqttConnectOptions()
-                try {
-                    mqttClient.connect(options, null, object : IMqttActionListener {
-                        override fun onSuccess(asyncActionToken: IMqttToken?) {
-                            Log.d(MainLogin.TAG, "Connection success")
-                        }
-
-                        override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
-                            Log.d(MainLogin.TAG, "Connection failure")
-                        }
-                    })
-                } catch (e: MqttException) {
-                    e.printStackTrace()
-                }
+    }
 
 
 
+    override fun updateStatusViewWith(status: String) {
+        TODO("Not yet implemented")
+    }
 
-
-        }
-
-
-
-        }
-
+    override fun update(message: String) {
+        TODO("Not yet implemented")
+    }
 
 }
 
---
+++ app/src/main/java/com/example/remotecontroller/MainRemote.kt
@@ -0,0 +1,6 @@
+package com.example.remotecontroller
+
+
+class MainRemote {
+
+}
